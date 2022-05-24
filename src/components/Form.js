231a73469932bfc3bfa4aa260d04cec9343441d4
Akkavin <vinarski.a@gmail.com>
2022-05-24 12:16:18 +0300

    fix tries

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,29 @@ import Input from "./Input";
 
 class Form extends React.Component {
 
+    constructor (props) {
+      super(props)
+      let formStateObj = {};
+      this.state = {};
+      // this.props.inputList.forEach ( (input, i) => {
+      formStateObj = this.props.inputList.reduce ( (target, key)  => {
+       // console.dir (input)
+        //return {...target,key: 0}
+        // console.log (Object.values(input))
+        /// const {id} = input;
+        // console.log (id)
+        /// const te  mpObj = { id: 1}
+        // console.log (tempObj)
+        // let id = input['id'];
+        // Object.assign (formStateObj, {id.toString():0})
+
+        // this.setState ({ 'abc': 0})
+        // this.setState ({...state, input[id]: ""})
+        
+      },{})
+      console.log (formStateObj)
+    }
+    
     render() {
 
       let formClassName = "form";
@@ -11,11 +34,11 @@ class Form extends React.Component {
           formClassName+= " form_type_delete-card";
 
       let valuesObj = null;
-      let refs = null;
+     // let refs = null;
       if (this.props.inputsState && Object.keys(this.props.inputsState).length > 0)
         valuesObj = this.props.inputsState;
-      else
-        refs = this.props.inputsRefs;
+      //else
+      //  refs = this.props.inputsRefs;
       return (
        
         <form 
@@ -29,11 +52,23 @@ class Form extends React.Component {
             {   this.props.inputList.map( input =>  {
 
                 return <React.Fragment key = {input.id}>
-                    <Input 
+                    {/* <Input 
                       input = {input}
                       onChange = {this.props.onChange}
                       valueData = {valuesObj ? valuesObj[input.id] : ""}
-                      refData = {refs ? refs[input.id] : null}
+             //         refData = {refs ? refs[input.id] : null}
+                    /> */}
+                    <input
+                      value = {valuesObj[input.id] }  // || ""}
+                      type = {input.type}
+                      id = {`${input.id}-input`}
+                      className= {`form__input form__input_type_${input.id}`}
+                      name = {input.id}
+                      placeholder = {input.placeholder}
+                      minLength = {input.minLength }
+                      maxLength = {input.maxLength }
+                      required = {true}
+                      onChange = {this.props.onChange}
                     />
                     <span id={`${input.id}-input-error`} className="form__error">&nbsp;</span>
                   </React.Fragment>
